3509e01fe777a0c8f8b55f2ea334c1de19133fb6
Linus Marton <linus.pettersson@gmail.com>
2020-11-29 20:05:54 +0100

    New layout

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -72,15 +72,15 @@ impl_heterogenous_array! {
 const CUT: Action = m(&[LShift, Delete]);
 const COPY: Action = m(&[LCtrl, Insert]);
 const PASTE: Action = m(&[LShift, Insert]);
-const L2_ENTER: Action = HoldTap {
+const L1_ENTER: Action = HoldTap {
     timeout: 140,
-    hold: &l(2),
+    hold: &l(1),
     tap: &k(Enter),
 };
-const L1_SP: Action = HoldTap {
-    timeout: 200,
-    hold: &l(1),
-    tap: &k(Space),
+const L2_BSP: Action = HoldTap {
+    timeout: 140,
+    hold: &l(2),
+    tap: &k(BSpace),
 };
 const CSPACE: Action = m(&[LCtrl, Space]);
 macro_rules! s {
@@ -97,15 +97,15 @@ macro_rules! a {
 #[rustfmt::skip]
 pub static LAYERS: keyberon::layout::Layers = &[
     &[
-        &[k(Tab),     k(Q), k(W),  k(E),    k(R), k(T),    k(Y),     k(U),    k(I),   k(O),    k(P),     k(LBracket)],
-        &[k(RBracket),k(A), k(S),  k(D),    k(F), k(G),    k(H),     k(J),    k(K),   k(L),    k(SColon),k(Quote)   ],
-        &[k(Equal),   k(Z), k(X),  k(C),    k(V), k(B),    k(N),     k(M),    k(Comma),k(Dot), k(Slash), k(Bslash)  ],
-        &[Trans,      Trans,k(LGui),k(LAlt),L1_SP,k(LCtrl),k(RShift),L2_ENTER,k(RAlt),k(BSpace),Trans,   Trans      ],
+        &[k(Tab),    k(Q), k(W),  k(E),    k(R),   k(T),   k(Y),     k(U),    k(I),    k(O),   k(P),     k(Equal) ],
+        &[k(LCtrl),  k(A), k(S),  k(D),    k(F),   k(G),   k(H),     k(J),    k(K),    k(L),   k(SColon),k(Quote) ],
+        &[k(LShift), k(Z), k(X),  k(C),    k(V),   k(B),   k(N),     k(M),    k(Comma),k(Dot), k(Slash), k(Bslash)],
+        &[Trans,     Trans,Trans, k(LAlt), k(LGUI),L2_BSP, L1_ENTER, k(Space),k(RAlt), Trans,  Trans,    Trans    ],
     ], &[
-        &[Trans,         k(Pause),Trans,     k(PScreen),Trans,    Trans,Trans,      Trans,  k(Delete),Trans,  Trans,   Trans ],
-        &[Trans,         Trans,   k(NumLock),k(Insert), k(Escape),Trans,k(CapsLock),k(Left),k(Down),  k(Up),  k(Right),Trans ],
-        &[k(NonUsBslash),k(Undo), CUT,       COPY,      PASTE,    Trans,Trans,      k(Home),k(PgDown),k(PgUp),k(End),  Trans ],
-        &[Trans,         Trans,   Trans,     Trans,     Trans,    Trans,Trans,      Trans,  Trans,    Trans,  Trans,   Trans ],
+        &[Trans,         k(Pause),Trans,     k(PScreen),Trans,    Trans,Trans,  k(RBracket), k(LBracket),Trans,   Trans,  Trans ],
+        &[Trans,         Trans,   k(NumLock),k(Insert), k(Escape),Trans,k(Left),k(Down),     k(Up),      k(Right),Trans,  Trans ],
+        &[k(NonUsBslash),k(Undo), CUT,       COPY,      PASTE,    Trans,Trans,  k(Home),     k(PgDown),  k(PgUp), k(End), Trans ],
+        &[Trans,         Trans,   Trans,     Trans,     Trans,    Trans,Trans,  Trans,       Trans,      Trans,   Trans,  Trans ],
     ], &[
         &[s!(Grave),s!(Kb1),s!(Kb2),s!(Kb3),s!(Kb4),s!(Kb5),s!(Kb6),s!(Kb7),s!(Kb8),s!(Kb9),s!(Kb0),s!(Minus)],
         &[ k(Grave), k(Kb1), k(Kb2), k(Kb3), k(Kb4), k(Kb5), k(Kb6), k(Kb7), k(Kb8), k(Kb9), k(Kb0), k(Minus)],
